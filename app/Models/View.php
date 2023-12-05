f827054709effa1f0dba2525118dc327d2f6ff28
vgianluca96 <gianluca.vallese@hotmail.com>
2023-12-05 18:34:04 +0100

    add models + migrations + realtions
    Added following tables: messages, views, services, apartments_services

---
+++ app/Models/View.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Apartment;
+
+class View extends Model
+{
+    use HasFactory;
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+}
