7388baec2ebf5812c283eb562ff47bf59f7b160a
vgianluca96 <gianluca.vallese@hotmail.com>
2023-12-06 16:59:35 +0100

    popolato seeder Messages

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $newmessage = new Message();
+            $newmessage->name = 'Gianluca';
+            $newmessage->lastname = 'Vallese';
+            $newmessage->email = 'email@example.it';
+            $newmessage->phone = '+39 340111222';
+            $newmessage->subject = 'Informazioni per appartamento 1';
+            $newmessage->message = 'Ciao, sono interessato a questo appartamento, è disponibile dal 25 dicembre al 1 gennaio compreso?';
+            $newmessage->apartment_id = 1;
+            $newmessage->save();
+        }
     }
 }
