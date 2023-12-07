f648e6a87dca693fb3ef03ebcdf26ee38ba3b705
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-12-07 10:42:53 +0100

    add tomtom logic Geocoding

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -17,6 +17,7 @@ class ApartmentSeeder extends Seeder
     {
         $response = Http::withoutVerifying()->get("https://3bd7e14b-0b57-4476-a3ec-2878f890c41f.mock.pstmn.io/https://idealista2.p.rapidapi.com/properties/list?locationId=0-EU-IT-MI&locationName=Milano&operation=rent&country=it&locale=it&maxItems=10");
 
+        $TomtomKey = 'zGXu3iFl86vJs8yD3Uq6OGoANFEGzFkS';
         if ($response->successful()) {
             $apartments = $response->json()['elementList'];
             foreach ($apartments as $apartment) {
@@ -29,9 +30,18 @@ class ApartmentSeeder extends Seeder
                 $NewApartment->rooms = $apartment['rooms'];
                 $NewApartment->bathrooms = $apartment['bathrooms'];
                 $NewApartment->square_meters = $apartment['size'];
-                $NewApartment->address = $apartment['address'];
-                $NewApartment->latitude = $apartment['latitude'];
-                $NewApartment->longitude = $apartment['longitude'];
+                $NewApartment->address = $apartment['address'] . ', ' .  $apartment['municipality'] . ', ' . $apartment['province'];
+
+                if (isset($apartment['address']) && $apartment['address'] !== null) {
+                    $address =  $NewApartment->address;
+                    $address = str_replace(' ', '%20', $address);
+                    $Geocoding = Http::withoutVerifying()->get("https://api.tomtom.com/search/2/geocode/" .  $address . ".json?key=" . $TomtomKey);
+                    if ($Geocoding->successful()) {
+                        $Coordinates = $Geocoding['results'][0]['position'];
+                        $NewApartment->latitude = $Coordinates['lat'];
+                        $NewApartment->longitude = $Coordinates['lon'];
+                    }
+                }
                 $NewApartment->is_visible = 1;
                 $NewApartment->save();
             }
