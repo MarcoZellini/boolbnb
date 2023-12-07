b0362795616fd673f5be2be07a5f24c0e0ddaa3f
vgianluca96 <gianluca.vallese@hotmail.com>
2023-12-07 10:46:10 +0100

    realizzata traduz messaggi errore

---
+++ lang/it/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Queste credenziali non sono presenti fra quelle registrate.',
+    'password' => 'La password fornita non è corretta.',
+    'throttle' => 'Effettuati troppi tentativi di login. Per favore riprovare tra :seconds secondi.',
+
+];
