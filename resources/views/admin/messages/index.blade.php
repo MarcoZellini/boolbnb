6040b7c9f82e04ef8e02bdbb4fb6bde1a0bdad1d
vgianluca96 <gianluca.vallese@hotmail.com>
2023-12-11 12:05:04 +0100

    restyling index apart e mex
    uniformati i bottoni e i font

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -55,23 +55,23 @@
                                             </div>
                                             <div class="modal-body">
                                                 <div class="mt-2">
-                                                    <h6><strong>Appartamento ID:
-                                                            {{ $message->apartment_id }}</h6>
+                                                    <h6><strong>Appartamento ID:</strong>
+                                                        {{ $message->apartment_id }}</h6>
                                                 </div>
                                                 <div class="mt-4">
                                                     <h6><strong>Dettagli del mittente:</strong></h6>
                                                     <ul class="list-unstyled m-0">
                                                         <li>
-                                                            Nome: {{ $message->name }}
+                                                            <strong>Nome:</strong> {{ $message->name }}
                                                         </li>
                                                         <li>
-                                                            Cognome: {{ $message->lastname }}
+                                                            <strong>Cognome:</strong> {{ $message->lastname }}
                                                         </li>
                                                         <li>
-                                                            Email: {{ $message->email }}
+                                                            <strong>Email:</strong> {{ $message->email }}
                                                         </li>
                                                         <li>
-                                                            Telefono: {{ $message->phone }}
+                                                            <strong>Telefono:</strong> {{ $message->phone }}
                                                         </li>
                                                     </ul>
                                                 </div>
@@ -101,36 +101,26 @@
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <h1 class="modal-title fs-5" id="deleteModalLabel">
-                                                    Eliminare messaggio n. {{ $message->id }}?
+                                                    Eliminare messaggio di {{ $message->name }}?
                                                 </h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                    fill="#FF0000" class="bi bi-exclamation-triangle-fill"
-                                                    viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                                                </svg>
+                                                <i class="fa-solid fa-circle-exclamation fa-lg" style="color: #e00b41;"></i>
                                                 <span class="mx-1">
                                                     <strong>Attenzione</strong>: questa azione è irreversibile
                                                 </span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                    fill="#FF0000" class="bi bi-exclamation-triangle-fill"
-                                                    viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                                                </svg>
                                             </div>
                                             <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
+                                                <button type="button"
+                                                    class="btn rounded-pill btn-outline-secondary btn-bnb-secondary"
                                                     data-bs-dismiss="modal">Close</button>
                                                 <form action="{{ route('admin.messages.delete', $message->id) }}"
                                                     method="POST">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                    <button type="submit" class="btn btn-bnb rounded-pill">Delete</button>
                                                 </form>
                                             </div>
                                         </div>
