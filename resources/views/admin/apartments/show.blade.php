63f28304c81656b50da0e2798ea6a50f38588f10
francescomascellino <franesco.mascellino@gmail.com>
2023-12-12 17:32:37 +0100

    Add w-100 on extra images in show view

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -49,7 +49,7 @@
 
                             {{-- APPLIES PADDING CLASSES BASED ON THE LOOP ITERATION --}}
                             <div class="col-6 m-0 p-1 h-50">
-                                <img class="h-100 img-fluid object-fit-cover rounded {{ $styleClass }} shadow"
+                                <img class="h-100 w-100 img-fluid object-fit-cover rounded {{ $styleClass }} shadow"
                                     src="{{ URL::asset('storage/' . $image->path) }}" alt="{{ $apartment->title }}">
                             </div>
                         @endif
