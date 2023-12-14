1b03aaa88308f10de30f011fa7170b78fb93ab95
francescomascellino <franesco.mascellino@gmail.com>
2023-12-14 11:20:54 +0100

    create view controller and form request

---
+++ app/Http/Controllers/API/ViewController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\View;
+use App\Http\Requests\StoreViewRequest;
+use App\Http\Requests\UpdateViewRequest;
+
+class ViewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreViewRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(View $view)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateViewRequest $request, View $view)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(View $view)
+    {
+        //
+    }
+}
