d4696aca10e2ae0f2a744aec28da5d5729f7481f
francescomascellino <franesco.mascellino@gmail.com>
2023-12-15 13:20:19 +0100

    Add back btns and layout fixes

--- resources/views/admin/apartments/images/index.blade.php
+++ resources/views/admin/apartments/images/index.blade.php
@@ -25,8 +25,6 @@
                 </div>
             @endif
 
-
-
             <br>
 
             <div class="col-12">
@@ -52,8 +50,6 @@
                                                 document.getElementById('set_main_{{ $image->id }}').submit();">Rendi
                                                     Principale</a>
 
-                                                {{-- <button type="submit" class="btn btn-success rounded-pill"
-                                                    form="set_main_{{ $image->id }}">Principale</button> --}}
                                             @endif
                                         </td>
                                         <td>
@@ -125,7 +121,7 @@
             <div class="col-12 mb-3">
 
                 <a class="btn btn-primary rounded-circle border bnb-btn-shadow bnb-btn-actions me-1 "
-                    href="{{ route('admin.apartments.index', $apartment) }}" role="button">
+                    href="{{ url()->previous() }}" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
@@ -155,98 +151,6 @@
                 </a>
             </div>
 
-            ORIGINAL
-            <div class="col-12">
-                <div class="col-12 mb-3">
-                    <h6>Gestisci immagini</h6>
-                    <div class="table-responsive">
-                        <table class="table table-info table-striped table-bordered border-dark align-middle text-center">
-                            <thead>
-                                <tr>
-                                    <th scope="col" class="text-uppercase">id</th>
-                                    <th scope="col" class="text-capitalize">Immagine</th>
-                                    <th scope="col" class="text-capitalize">principale</th>
-                                    <th scope="col" class="text-capitalize">azioni</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($apartment->images as $image)
-                                    <tr>
-                                        <td>{{ $image->id }}</td>
-                                        <td>
-                                            <img height="50px" src="{{ asset('storage/' . $image->path) }}"
-                                                alt="">
-                                        </td>
-                                        <td>
-
-                                            @if ($image->is_main)
-                                                <span>Si</span>
-                                            @else
-                                                <button type="submit" class="btn btn-success"
-                                                    form="set_main_{{ $image->id }}">Imposta
-                                                    Principale</button>
-                                            @endif
-                                        </td>
-                                        <td>
-
-                                            <!-- Modal trigger button -->
-                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                                data-bs-target="#modalId-{{ $image->id }}">
-                                                Elimina
-                                            </button>
-
-                                            <!-- Modal Body -->
-                                            <div class="modal fade" id="modalId-{{ $image->id }}" tabindex="-1"
-                                                role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-                                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                                    role="document">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="modalTitleId">
-                                                                Eliminare immagine #{{ $image->id }}
-                                                            </h5>
-                                                            <button type="button" class="btn-close"
-                                                                data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">Vuoi davvero eliminare questa immagine?
-                                                            Quest'azione non e' reversibile.</div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary"
-                                                                data-bs-dismiss="modal">
-                                                                Close
-                                                            </button>
-                                                            {{-- TODO Impostare la action per eliminare la foto --}}
-
-                                                            <button type="submit" class="btn btn-danger"
-                                                                form="delete_image_{{ $image->id }}">Elimina</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4">
-                                            Non ci sono immagini
-                                        </td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12">
-                <a name="" id="" class="btn btn-success" href="{{ url()->previous() }}"
-                    role="button">Back
-                    To
-                    Apartments</a>
-                <a name="" id="" class="btn btn-primary"
-                    href="{{ route('admin.apartments.show', $apartment) }}" role="button">Show
-                    Apartment</a>
-            </div>
         </div>
     </div>
 @endsection
